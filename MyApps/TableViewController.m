4e593b1741d3a826dde50d13fa935c6aa571d19f
Guilherme Ferreira de Souza <guilherme.souza94@hotmail.com>
2015-02-25 17:18:44 -0300

    Implementação de deleção.

--- MyApps/TableViewController.m
+++ MyApps/TableViewController.m
@@ -74,25 +74,30 @@
 }
 
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
+        
+        [_Nome removeObjectAtIndex:[indexPath row]];
+        [_Categoria removeObjectAtIndex:[indexPath row]];
+        [_Imagem removeObjectAtIndex:[indexPath row]];
+        
         // Delete the row from the data source
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    }
 }
-*/
 
 /*
 // Override to support rearranging the table view.
